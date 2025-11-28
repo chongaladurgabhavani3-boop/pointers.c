1c291be275a3f7648ed9832da6957762c4548904
chongaladurgabhavani3-boop <chongaladurgabhavani3@gmail.com>
2025-11-28 11:52:29 +0530

    Add files via upload

---
+++ call by value.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+void swap(int ,int);
+int main()
+{
+	int a,b;
+	printf("Enter a and b values:");
+	scanf("%d %d",&a,&b);
+	printf("\n Before main() swap a=%d,b=%d",a,b);
+	swap(a,b);
+	printf("\n After main() swap a=%d,b=%d",a,b);
+}
+void swap(int a,int b)
+{
+	int temp = a;
+	a=b;
+	b=temp;
+	printf("\n Inside swap() function a=%d,b=%d",a,b);
+}
